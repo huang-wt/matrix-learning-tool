707a42e5584eed8c5899a2a3f3c9c6325e787f0f
Wentao Huang <84852094+huang-wt@users.noreply.github.com>
2022-03-21 11:41:29 +0000

    check functionality added

--- exercise_generator.py
+++ exercise_generator.py
@@ -5,9 +5,11 @@ class ExercisesGenerator:
     EXERCISE_TYPES = {0:"add", 1:"sub", 2:"mult", 3:" determinant", 4:" inverse", 5:" eigenvalue(s)", 6:" eigenvector(s)"}
     EXERCISES_NUMBER = 5
 
-    def __init__(self, is_auto) -> None:
+    def __init__(self) -> None:
         self.exercises = []
-        
+
+    def set_exercises(self, is_auto):
+        self.exercises = []
         if is_auto:
             self.set_exercises_type()
             self.set_exercises_data()
@@ -18,24 +20,25 @@ class ExercisesGenerator:
         for i in range(self.EXERCISES_NUMBER):
             num = random.randint(0, 6)
             self.exercises.append([])
-            self.exercises[i].append(num)
+            self.exercises[i].append(num <= 2)
             self.exercises[i].append(self.EXERCISE_TYPES[num])
 
     def set_exercises_data(self):
+        
         for i in range(self.EXERCISES_NUMBER):
             if self.exercises[i][0]:
                 A = [random.randint(-20, 20) for _ in range(9)]
                 matrix_A = Matrix(A, self.exercises[i][1])
                 B = [random.randint(-20, 20) for _ in range(9)]
                 matrix_B = Matrix(B, self.exercises[i][1])
-                res = matrix_A.get_bi_result(matrix_B)
+                res = str(matrix_A.get_bi_result(matrix_B))
                 self.exercises[i].append(A)
                 self.exercises[i].append(B)
                 self.exercises[i].append(res)
             else:
                 A = [random.randint(-20, 20) for _ in range(9)]
                 matrix_A = Matrix(A, self.exercises[i][1])
-                res = matrix_A.get_result()
+                res = str(matrix_A.get_result())
                 self.exercises[i].append(A)
                 self.exercises[i].append(res)
 
@@ -64,7 +67,7 @@ class ExercisesGenerator:
                     B = list(map(int, lines[j].split(',')))
                     matrix_B = Matrix(B, self.exercises[i][1])
                     j += 1
-                    res = matrix_A.get_bi_result(matrix_B)
+                    res = str(matrix_A.get_bi_result(matrix_B))
                     self.exercises[i].append(A)
                     self.exercises[i].append(B)
                     self.exercises[i].append(res)
@@ -72,11 +75,20 @@ class ExercisesGenerator:
                     A = list(map(int, lines[j].split(',')))
                     matrix_A = Matrix(A, self.exercises[i][1])
                     j += 1
-                    res = matrix_A.get_result()
+                    res = str(matrix_A.get_result())
                     self.exercises[i].append(A)
                     self.exercises[i].append(res)
 
+    def check_answers(self, user_answers):
+        check = []
+        for i in range(self.EXERCISES_NUMBER):
+            check.append(user_answers[i] == self.exercises[i][-1])
+        return check
+
+
 
 # Imformal Test
-# eg = ExercisesGenerator(False)
+# eg = ExercisesGenerator()
+# eg.set_exercises(True)
 # print(eg.exercises)
+# print(eg.check_answers(["", "", "", "", '[[10, 20, 0], [5, 25, -2], [-21, 4, 4]]']))
